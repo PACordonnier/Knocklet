05853ea8d6797faeba4719b4f0a007dce1a0e3b4
Paul-Adrien Cordonnier <pa.cordonnier@gmail.com>
2017-01-26 16:54:36 +0100

    Ajout du delay + adresse bluetooth

--- go/knocklet.go
+++ go/knocklet.go
@@ -21,6 +21,7 @@ import (
 
 var done = make(chan struct{})
 var lastTime time.Time = time.Now()
+var ownBDAddress string
 
 type Params struct {
 	Apikey string `json:"apikey"`
@@ -35,6 +36,19 @@ type Payload struct {
 	Jsonrpc string `json:"jsonrpc"`
 	Params Params `json:"params"`
 }
+
+type Error struct {
+	Code int `json:"code"`
+	Message string `json:"message"`
+}
+
+type Response struct {
+	Jsonrpc string `json:"jsonrpc"`
+	Id int `json:"id"`
+	Result string `json:"result"`
+	Error Error `json:"error"`
+}
+
 
 func onStateChanged(d gatt.Device, s gatt.State) {
 	fmt.Println("State:", s)
@@ -81,7 +95,10 @@ func sendToJeedom(braceletId string, moduleId string, knocks int, rssi int) {
 	req.Header.Set("Content-Type",  "application/json")
 	client := &http.Client{}
 	resp, err := client.Do(req)
+	response := Response{}
+	_ = json.NewDecoder(resp.Body).Decode(&response)
 	fmt.Println(string(b))
+	fmt.Println(response)
 	if err != nil {
 		panic(err)
 	}
@@ -93,7 +110,7 @@ func onPeriphDiscovered(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
 	if strings.ToUpper(p.ID()) != id {
 		return
 	}
-	fmt.Println("coucou")
+	fmt.Println(rssi)
 	if (time.Since(lastTime).Seconds() > 2.55555) {
 		fmt.Printf("\nPeripheral ID:%s, NAME:(%s)\n", p.ID(), p.Name())
 		fmt.Println("  Local Name        =", a.LocalName)
@@ -105,9 +122,33 @@ func onPeriphDiscovered(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
 		lastTime = time.Now()
 		h := []byte{0}
 		val := append(h, a.ServiceData[0].Data[0])
-		sendToJeedom(p.ID(), "5555555", int(binary.BigEndian.Uint16(val)), rssi)
+		time.Sleep(time.Duration(-rssi) * time.Millisecond)
+		sendToJeedom(p.ID(), ownBDAddress, int(binary.BigEndian.Uint16(val)), rssi)
 		fmt.Println("end")
 	}
+}
+
+// cmdReadBDAddr implements cmd.CmdParam for demostrating LnxSendHCIRawCommand()
+type cmdReadBDAddr struct{}
+
+func (c cmdReadBDAddr) Marshal(b []byte) {}
+func (c cmdReadBDAddr) Opcode() int      { return 0x1009 }
+func (c cmdReadBDAddr) Len() int         { return 0 }
+
+
+// Get bdaddr with LnxSendHCIRawCommand() for demo purpose
+func bdaddr(d gatt.Device) string {
+	rsp := bytes.NewBuffer(nil)
+	if err := d.Option(gatt.LnxSendHCIRawCommand(&cmdReadBDAddr{}, rsp)); err != nil {
+		fmt.Printf("Failed to send HCI raw command, err: %s", err)
+	}
+	b := rsp.Bytes()
+	if b[0] != 0 {
+		fmt.Printf("Failed to get bdaddr with HCI Raw command, status: %d", b[0])
+	}
+	log.Printf("BD Addr: %02X:%02X:%02X:%02X:%02X:%02X", b[6], b[5], b[4], b[3], b[2], b[1])
+	s := fmt.Sprintf("%02X:%02X:%02X:%02X:%02X:%02X", b[6], b[5], b[4], b[3], b[2], b[1])
+	return s
 }
 
 func main() {
@@ -121,7 +162,8 @@ func main() {
 		log.Fatalf("Failed to open device, err: %s\n", err)
 		return
 	}
-
+	ownBDAddress = bdaddr(d)
+	fmt.Println(ownBDAddress)
 	// Register handlers.
 	d.Handle(
 		gatt.PeripheralDiscovered(onPeriphDiscovered),

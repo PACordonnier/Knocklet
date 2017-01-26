c4b7a16c597f8b9961e52d0526d2474d9034ebc6
Paul-Adrien Cordonnier <pa.cordonnier@gmail.com>
2017-01-26 10:52:07 +0100

    Dernier commit python

--- python/broadcast.py
+++ python/broadcast.py
@@ -1,31 +1,34 @@
 from bluepy.btle import Scanner, DefaultDelegate, Peripheral
 from uuid import getnode as get_mac
+import simplejson
 import json
 import requests
 
 url = "http://jeedom.local/plugins/knocklet/core/api/knocklet.api.php"
 headers = {'content-type': 'application/json'}
 
-payload = {
-  "method": "knock",
-  "jsonrpc": "2.0",
-  "params": {
-        "apikey": "lbippE2VJkuxVdfI4sKtZWQkCTwNJxH517kYVYEDLBbUe2eD",
-        "id":8,
-        "braceletId":1,
-        "moduleId":1,
-        "knocks":1,
-        "rssi":1
-  }
-}
-payload["params"]["moduleId"] = get_mac()
-print payload
-
 def postDatas(deviceAddr, knocks, rssi):
-    payload["params"]["knocks"] = knocks
-    payload["params"]["braceletId"] = deviceAddr
-    payload["params"]["rssi"] = rssi
-    requests.post(url, data = json.dumps(payload), headers=headers).json()
+    payload = {
+      "method": "knock",
+      "jsonrpc": "2.0",
+      "params": {
+            "apikey": "lbippE2VJkuxVdfI4sKtZWQkCTwNJxH517kYVYEDLBbUe2eD",
+            "braceletId":1,
+            "moduleId":1,
+      }
+    }
+    if(knocks == 'ff'):
+        payload["method"] = 'init'
+    else:
+        payload["params"]["knocks"] = knocks
+        payload["params"]["rssi"] = str(rssi)
+    payload["params"]["moduleId"] = str(get_mac())
+    payload["params"]["braceletId"] = deviceAddr.encode('utf-8')
+    print "Sending  datas" + knocks
+    print payload
+    data = json.dumps(payload)
+    requests.post(url, data = data, headers=headers).json()
+    print "data sent"
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
@@ -35,7 +38,7 @@ class ScanDelegate(DefaultDelegate):
         if (dev.addr == "4b:4e:4f:00:00:02" or dev.addr == "4b:4e:4f:00:00:01"): 
             if isNewDev:
                 print "Discovered device", dev.addr
-                knocks =  dev.getValueText(33)[-2:]
+                knocks =  dev.getValueText(33)[-2:].encode('utf-8')
                 deviceAddr = dev.addr
                 scanner.clear()
                 postDatas(deviceAddr, knocks, dev.rssi) 
